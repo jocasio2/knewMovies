04c1d0b5f58b8fed8d1d36bd6e28f9ac2afdfcf3
jocasio2 <joeyohh211@gmail.com>
2023-03-21 15:18:51 -0600

    some text

---
+++ knex-demo/knexfile.js
@@ -0,0 +1,47 @@
+// Update with your config settings.
+
+const { password, database } = require("pg/lib/defaults");
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://postgres:docker@localhost/movies'
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
